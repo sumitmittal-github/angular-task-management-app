1d07d9ef93d1b04d5175cb9ab813a9ee416050ce
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-05-17 16:22:57 -0400

    added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [HeaderComponent]
 })
 export class AppComponent {
+  
   title = 'first-angular-app';
+
 }
