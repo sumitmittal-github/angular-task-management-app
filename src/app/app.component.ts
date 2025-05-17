9f6a6e4dc2f3e97fed22677f5c205bc98ed1c279
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-05-17 18:18:55 -0400

    Added users component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
   
