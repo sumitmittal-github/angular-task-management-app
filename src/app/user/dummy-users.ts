e0e520f2e5800fa7b90349039bccac020dd9ab66
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-05-17 19:06:51 -0400

    code modified for property binding

--- src/app/user/dummy-users.ts
+++ src/app/user/dummy-users.ts
@@ -2,31 +2,31 @@ export const DUMMY_USERS = [
     {
       id: 'u1',
       name: 'Jasmine Washington',
-      avatar: '/users/user-1.jpg',
+      avatar: 'user-1.jpg',
     },
     {
       id: 'u2',
       name: 'Emily Thompson',
-      avatar: '/users/user-2.jpg',
+      avatar: 'user-2.jpg',
     },
     {
       id: 'u3',
       name: 'Marcus Johnson',
-      avatar: '/users/user-3.jpg',
+      avatar: 'user-3.jpg',
     },
     {
       id: 'u4',
       name: 'David Miller',
-      avatar: '/users/user-4.jpg',
+      avatar: 'user-4.jpg',
     },
     {
       id: 'u5',
       name: 'Priya Patel',
-      avatar: '/users/user-5.jpg',
+      avatar: 'user-5.jpg',
     },
     {
       id: 'u6',
       name: 'Arjun Singh',
-      avatar: '/users/user-6.jpg',
+      avatar: 'user-6.jpg',
     },
   ];
