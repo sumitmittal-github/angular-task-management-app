1d9b4e667a4fa661f9735d59abe762707961b867
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-05-18 19:43:08 -0400

    code refactored

--- src/app/user/dummy-users.ts
+++ src/app/user/dummy-users.ts
@@ -2,31 +2,31 @@ export const DUMMY_USERS = [
     {
       id: 'u1',
       name: 'Jasmine Washington',
-      avatar: 'user-1.jpg',
+      image: 'user-1.jpg',
     },
     {
       id: 'u2',
       name: 'Emily Thompson',
-      avatar: 'user-2.jpg',
+      image: 'user-2.jpg',
     },
     {
       id: 'u3',
       name: 'Marcus Johnson',
-      avatar: 'user-3.jpg',
+      image: 'user-3.jpg',
     },
     {
       id: 'u4',
       name: 'David Miller',
-      avatar: 'user-4.jpg',
+      image: 'user-4.jpg',
     },
     {
       id: 'u5',
       name: 'Priya Patel',
-      avatar: 'user-5.jpg',
+      image: 'user-5.jpg',
     },
     {
       id: 'u6',
       name: 'Arjun Singh',
-      avatar: 'user-6.jpg',
+      image: 'user-6.jpg',
     },
   ];
