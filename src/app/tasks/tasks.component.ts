8b88516f35423e0a8ab8bbde29743d916424a891
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-05-19 19:55:09 -0400

    Added Task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../models/user';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
