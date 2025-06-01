1f15357bf5d2fd106bdd7db58412e41110647bb0
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-06-01 15:31:56 -0400

    Local Storage integrated for tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,6 +7,17 @@ export class TasksService {
 
     private tasks: Task[] = DUMMY_TASKS;
 
+    constructor() {
+        const tasksFromLocalStorage = localStorage.getItem('tasks');
+        if (tasksFromLocalStorage) {
+            this.tasks = JSON.parse(tasksFromLocalStorage);
+        }
+    }
+
+    private refreshLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
 
 
     getUserTasks(userId: number) {
@@ -17,10 +28,12 @@ export class TasksService {
         newTask.id = new Date().getTime();  // generate a unique ID based on the current timestamp
         newTask.userId = userId;            // ensure the task is associated with the selected user
         this.tasks.push(newTask);
+        this.refreshLocalStorage();
     }
 
     completeTask(taskId: number) {
         this.tasks = this.tasks.filter(t => t.id !== taskId);
+        this.refreshLocalStorage();
     }
 
 
